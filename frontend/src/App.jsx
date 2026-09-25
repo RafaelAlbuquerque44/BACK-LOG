ba77b82cb211ef97f597e4f364ef4b9d23fbad03
Rafael Albuquerque <rafael@example.com>
2026-09-24 21:52:44 -0300

    fix: App.jsx useEffect import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Vendas from './pages/Vendas';
